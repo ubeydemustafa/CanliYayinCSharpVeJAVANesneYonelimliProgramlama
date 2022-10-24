f89bab7859e6326bb24eb91ea1cfa8a4f5b88caa
ubeyde <mustafaubeydeezici@gmail.com>
2022-10-23 22:24:09 -0400

    frist commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int sayi1 = 10;
+        int sayi2 = 20;
+        sayi1 = sayi2;
+        sayi2 = 100;
+
+        System.out.println(sayi2);
+
+
+        // ikinci ornek
+        int[] sayilar1 = new int[]{1,2,3};
+        int[] sayilar2 = new int[]{10,20,30};
+        sayilar1 = sayilar2;
+        sayilar2[0]= 1000;
+        System.out.println(sayilar1[0]);
+
+
+        CreditManager creditManager = new CreditManager();
+        creditManager.Calculate();
+        creditManager.Save();
+
+        Customer customer = new Customer();
+        customer.Id = 10;
+        customer.FirstName ="ubeyde";
+        customer.LastName ="Ezici";
+        customer.NationalIdentity ="124235436534";
+        customer.City = "Ankara";
+
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Save(customer);
+        customerManager.Save(customer);
+        customerManager.Save(customer);
+        customerManager.Save(customer);
+
+
+
+    }
+
+
+}
